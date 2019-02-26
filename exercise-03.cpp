4faa9664ed62cc86330716a36bd09d7a10fe0d69
FarizAlfairuz <farizalfairuz27@gmail.com>
2019-02-26 09:53:32 +0700

    third commit

---
+++ exercise-03.cpp
@@ -0,0 +1,23 @@
+/*
+Nama			: Falah Rizqi Abdullah Fairuz
+NPM				: 140810180069
+Nama Program	: Swap
+Tanggal Buat 	: 26 Februari 2019
+*/
+
+#include<iostream>
+using namespace std;
+
+void swap(){
+}
+
+main()
+{
+	int x, y;
+	cout << "x = "; cin >> x;
+	cout << "y = "; cin >> y;
+	swap(x,y);
+	cout << "Setelah swap" << endl;
+	cout << "x = " << x;
+	cout << "y = " << y;
+}
